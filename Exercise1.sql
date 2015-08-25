2d8d1ac98f36e2ca2e55b311ea72269c24ff335d
Daniel Okwufulueze <daniel@dealdey.com>
2015-08-25 16:49:30 +0100

    Manipulate testing_table

---
+++ Exercise1.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS mysql_track;
+
+USE mysql_track;
+
+CREATE TABLE IF NOT EXISTS testing_table (
+  name VARCHAR(200),
+  contact_name VARCHAR(200),
+  roll_no char(10)
+);
+
+ALTER TABLE testing_table 
+  DROP name, 
+  CHANGE contact_name username VARCHAR(200), 
+  ADD first_name VARCHAR(100), 
+  ADD last_name VARCHAR(100), 
+  MODIFY roll_no INT;
